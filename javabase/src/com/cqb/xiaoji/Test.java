4b99e43f19d8a3838f20d2026ba3edce5feb5a8d
chenqingbao <943578400@qq.com>
2016-11-04 13:38:20 +0800

    Signed-off-by: chenqingbao <943578400@qq.com>

--- javabase/src/com/cqb/xiaoji/Test.java
+++ javabase/src/com/cqb/xiaoji/Test.java
@@ -8,11 +8,24 @@
 
 package com.cqb.xiaoji;
 
+import java.util.Scanner;
+
 public class Test {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-      
-	}
+		Scanner input = new Scanner(System.in);
+		System.out.println("请输入您要买的只数");
+		int num = input.nextInt();
+		System.out.println("请输入您准备花多少钱");
+		int money = input.nextInt();
+		System.out.println("您可购买： ");
 
+		for (int gongji = 0; gongji <= money / 5; ++gongji) {
+			int muji = (3 * money - num - 14 * gongji) / 8;
+			int xiaoji = num - gongji - muji;
+			if (5 * gongji + 3 * muji + xiaoji / 3 == money && muji >= 0 && xiaoji >= 0) {
+				System.out.println("公鸡有 ：" + gongji + "只" + "    母鸡有 ： " + muji + "只" + "     小鸡有 ： " + xiaoji + "只");
+			}
+		}
+
+	}
 }
