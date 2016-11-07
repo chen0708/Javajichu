a97f9032f12a0e0e9032d298951d8145f09f05ec
chenqingbao <943578400@qq.com>
2016-11-08 00:53:18 +0800

    Signed-off-by: chenqingbao <943578400@qq.com>

--- javabase/src/com/cqb/io/FileReaderDemo.java
+++ javabase/src/com/cqb/io/FileReaderDemo.java
@@ -1,40 +1,57 @@
 package com.cqb.io;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FileReaderDemo {
 
 	public static void main(String[] args) throws IOException {
-		File file1 = new File("d:\\demo\\test1.txt");
+		File file1 = new File("d:\\demo\\test.txt");
 		/*
-		 * 第一种创建文件目录和文件方法
+		 * 绗竴绉嶅垱寤烘枃浠剁洰褰曞拰鏂囦欢鏂规硶
 		 */
 		if (!file1.exists()) {
 			File parentsPath = file1.getParentFile();
 			parentsPath.mkdirs();
 			boolean b = file1.createNewFile();
-			System.out.println("是否创建成功 : " + b);
+			System.out.println("鏄惁鍒涘缓鎴愬姛 : " + b);
 		}
 		/*
-		 * 第二种创建文件目录和文件方法 File file2 = new File(file1.getParent());
+		 * 绗簩绉嶅垱寤烘枃浠剁洰褰曞拰鏂囦欢鏂规硶 File file2 = new File(file1.getParent());
 		 * file2.mkdirs(); boolean b = file1.createNewFile();
-		 * System.out.println("是否创建成功 : " + b);
+		 * System.out.println("鏄惁鍒涘缓鎴愬姛 : " + b);
 		 */
+
 		FileWriter fw = new FileWriter(file1);
-		FileReader fr=new FileReader(file1);
-		fw.write("这是一个Java上机题的文档文件： \r\n");
-		fw.write("一个正整数，它加上100后是一个完全平方数，再加上168又是一个完全平方数请问这个数是多少？ \r\n");
-		fw.write("需要的参数如下，通过程序读取文本文件，解析下面的字符串得到需要的参数值 \r\n");
+		FileInputStream fis = new FileInputStream(file1);
+		InputStreamReader isr = new InputStreamReader(fis, "GBK");
+		BufferedReader br = new BufferedReader(isr);
+		// FileReader fr=new FileReader(file1);
+
+		fw.write("杩欐槸涓�涓狫ava涓婃満棰樼殑鏂囨。鏂囦欢锛� \r\n");
+		fw.write("涓�涓鏁存暟锛屽畠鍔犱笂100鍚庢槸涓�涓畬鍏ㄥ钩鏂规暟锛屽啀鍔犱笂168鍙堟槸涓�涓畬鍏ㄥ钩鏂规暟璇烽棶杩欎釜鏁版槸澶氬皯锛� \r\n");
+		fw.write("闇�瑕佺殑鍙傛暟濡備笅锛岄�氳繃绋嬪簭璇诲彇鏂囨湰鏂囦欢锛岃В鏋愪笅闈㈢殑瀛楃涓插緱鍒伴渶瑕佺殑鍙傛暟鍊� \r\n");
 		fw.write("100,168 \r\n");
-		fw.write("解题思路：新建一个txt文件，利用FileWriter方法将题目信息写入至文件中，\r\n ");
-		fw.write("从文件中读取数值并进行解析，计算结果并写入文件中\r\n ");
+		fw.write("瑙ｉ鎬濊矾锛氭柊寤轰竴涓猼xt鏂囦欢锛屽埄鐢‵ileWriter鏂规硶灏嗛鐩俊鎭啓鍏ヨ嚦鏂囦欢涓紝\r\n ");
+		fw.write("浠庢枃浠朵腑璇诲彇鏁板�煎苟杩涜瑙ｆ瀽锛岃绠楃粨鏋滃苟鍐欏叆鏂囦欢涓璡r\n ");
 		fw.close();
-		
-		
-		
+		String str;
+		while ((str = br.readLine()) != null) {
+			String s = "\\d+.\\d+|\\w+|\\d+";
+			Pattern pattern = Pattern.compile(s);
+			Matcher ma = pattern.matcher(str);
+			while (ma.find()) {
+
+				 ma.group();
+			}
+
+		}
 
 	}
 
